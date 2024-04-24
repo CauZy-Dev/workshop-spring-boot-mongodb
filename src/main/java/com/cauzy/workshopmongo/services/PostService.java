a19f69605e99022f9fe1f89c2bd600e6a2571424
Cauã Farias <cauafariasdev@gmail.com>
2024-04-24 14:14:42 -0300

    feat: add postService and postResource

---
+++ src/main/java/com/cauzy/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cauzy.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cauzy.workshopmongo.domain.Post;
+import com.cauzy.workshopmongo.exception.ObjectNotFoundException;
+import com.cauzy.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+}
