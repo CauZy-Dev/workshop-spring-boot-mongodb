09c175b6fd44c4ffca223b69e3eb4ad57eaae5ab
Cauã Farias <cauafariasdev@gmail.com>
2024-04-24 14:52:30 -0300

    feat: add findByTitle method

--- src/main/java/com/cauzy/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cauzy/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.cauzy.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cauzy.workshopmongo.domain.Post;
 
-public interface PostRepository extends MongoRepository<Post, String>{
 
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
